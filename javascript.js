9ac26c5e383e203d5249b62f2d000e6e51ea8515
katelyj <kjohnston@stuy.edu>
2017-02-15 14:28:32 -0500

    hehe

--- javascript.js
+++ javascript.js
@@ -1,9 +1,26 @@
-
 var ctx = c.getContext("2d");
 
-c.addEventListener("click", function(e) {
-    ctx.fillRect(e.pageX, e.pageY, 50, 50);});
 
-b.addEventListener("click", function(e) {
-    ctx.clearRect( 0, 0, c.width, c.height);
-});
+var rec = function(e) {
+	ctx.fillRect(e.pageX, e.pageY, 50, 50);	
+};
+
+var clear = function(e) {
+	e.preventDefault(); 
+	ctx.clearRect( 0, 0, c.width, c.height);	
+	ctx.beginPath();
+
+};
+
+var cir = function(e) {
+	ctx.beginPath();
+	ctx.arc(e.offsetX, e.offsetY, 15, 0, 2 * Math.PI);
+	ctx.moveTo(e.offsetX, e.offsetY); 
+	ctx.fill();
+	ctx.stroke();
+};
+
+
+c.addEventListener("click", cir);
+
+b.addEventListener("click", clear);
